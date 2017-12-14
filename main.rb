7388cb6bd22abc863be97f347bace8e94fff5676
NilMV <nil.medvedev17@gmail.com>
2017-12-15 00:49:59 +0300

    experiments with telegram bot

---
+++ main.rb
@@ -0,0 +1,23 @@
+require 'telegram/bot'
+require 'token' #file with token
+
+
+ANSWERS = [
+  "PRIVET"
+]
+
+Telegram::Bot::Client.run(TOKEN) do |bot|
+  bot.listen do |message|
+    case message.text
+    when '/start', '/start start'
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: "Hello, #{message.from.first_name}")
+    else
+      bot.api.send_message(
+        chat_id: message.chat.id,
+        text: ANSWERS.sample
+      )
+    end
+  end
+end
